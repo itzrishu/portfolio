01eb11845723cf27d1ca5ef92a3dd3112b976ab4
shubham <shubhamrajshu000@gmail.com>
2026-03-10 03:46:12 +0530

    Initial commit of portfolio project

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Hi! I'm Shubham Raj, a passionate developer pursuing a B.Tech in Computer
+          Science and Engineering. I love building web applications and solving
+          problems. I am constantly learning and exploring new technologies to
+          create efficient, user-friendly digital experiences.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,69 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=shubhamrajshu000@gmail.com" data-cursor="disable">
+                shubhamrajshu000@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>BTech in Computer Science Engineering</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/itzrishu/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/shubham-raj-09aa69289/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/Shubham17220189"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/shubham__rajput__824?igsh=M3g1ajJkNHhsdjM4&utm_source=qr"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Shubham Raj</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2025
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
